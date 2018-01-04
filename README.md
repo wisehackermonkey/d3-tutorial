3fc2d36eaa777ac8ddb37e2734a343e411636ce7
Oran C <oranbusiness@gmail.com>
2018-01-04 13:53:53 -0800

    added readme, and d3 lib, and init html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-d3-tutorial
+# d3-tutorial
+### Author: wisemonkey
+Date: 20180104
+
+simple d3 tutorial to visualize data
---
+++ sketch.js
@@ -0,0 +1,12 @@
+/*
+Name: d3-tutorial
+Author: wisemonkey
+Date: 20180104
+*/
+function setup() {
+	createCanvas(600,400);
+	background(50);
+}
+
+function draw() {
+}
